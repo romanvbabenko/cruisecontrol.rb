e0113f675611f7ae154433868df58916a70def88
Roman V. Babenko <romanvbabenko@gmail.com>
2011-09-16 10:05:06 +0300

    [^] tests was fixed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,11 +33,11 @@ class ActiveSupport::TestCase
     exception = assert_raise(*args, &block)
     assert_match matcher, exception.message, message
   end
-  
+
   def assert_false(expression)
     assert_equal false, expression
   end
-  
+
   def in_total_sandbox(&block)
     in_sandbox do |sandbox|
       @dir = File.expand_path(sandbox.root)
@@ -47,21 +47,22 @@ class ActiveSupport::TestCase
       yield(sandbox)
     end
   end
-  
+
   def with_sandbox_project(&block)
     in_total_sandbox do |sandbox|
       FileUtils.mkdir_p("#{sandbox.root}/work/.svn")
-      
+
       project = Project.new(:name => 'my_project')
       project.path = sandbox.root
-      
+
       yield(sandbox, project)
     end
   end
-  
+
   def create_project_stub(name, last_complete_build_status = 'failed', last_five_builds = [])
     project = Object.new
     project.stubs(:name).returns(name)
+    project.stubs(:display_name).returns(name.humanize)
     project.stubs(:last_complete_build_status).returns(last_complete_build_status)
     project.stubs(:last_five_builds).returns(last_five_builds)
     project.stubs(:builder_state_and_activity).returns('building')
@@ -70,22 +71,22 @@ class ActiveSupport::TestCase
     project.stubs(:to_param).returns(name)
     project.stubs(:path).returns('.')
     project.stubs(
-      :building? => false, 
-      :sleeping? => true, 
+      :building? => false,
+      :sleeping? => true,
       :can_build_now? => true,
       :can_kill_builder? => true,
-      :previously_built? => false, 
+      :previously_built? => false,
       :builder_down? => false
     )
-    
+
     project.stubs(:last_complete_build).returns(nil)
     last_five_builds.reverse.each do |build|
       project.stubs(:last_complete_build).returns(build) unless build.incomplete?
     end
-    
+
     project
   end
-  
+
   def create_build_stub(label, status, time = Time.at(0))
     build = Object.new
     build.stubs(:label).returns(label)
