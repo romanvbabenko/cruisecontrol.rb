c8457146149a2e2b37ff0593f3f5e1832aa9fcaa
Roman V. Babenko <romanvbabenko@gmail.com>
2011-09-16 13:37:35 +0300

    [^] build_label helper with time zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,11 +22,11 @@ module ApplicationHelper
   def format_time(time, format = :iso)
     I18n.l time, :format => format
   end
-  
+
   def format_seconds(total_seconds, format = :general)
     DurationFormatter.new(total_seconds).send(format)
   end
-  
+
   def setting_row(label, value, help = '&nbsp;')
     <<-EOL
     <tr>
@@ -59,11 +59,11 @@ module ApplicationHelper
     end
     return text.html_safe
   end
-  
+
   def link_to_build_with_elapsed_time(project, build)
     build_link(build_to_text(build), project, build)
   end
-    
+
   def display_builder_state(state)
     case state
     when 'building', 'builder_down', 'build_requested', 'source_control_error', 'queued', 'timed_out', 'error'
@@ -78,7 +78,7 @@ module ApplicationHelper
   def format_changeset_log(log)
     h(log.strip)
   end
-  
+
   def elapsed_time(build, format = :general)
     begin
       content_tag :span, format_seconds(build.elapsed_time, format)
@@ -86,12 +86,12 @@ module ApplicationHelper
       '' # The build time is not present.
     end
   end
-  
+
   def build_link(text, project, build)
     link_to text, build_path(:project => project.name, :build => build.label),
             :class => build.status, :title => format_changeset_log(build.changeset)
   end
-  
+
   def url_path(url)
     if url.is_a?(Hash)
       url_for(url.merge(:only_path => true))
@@ -103,11 +103,11 @@ module ApplicationHelper
   def button_tag(label, attrs={})
     content_tag :button, label, attrs
   end
-        
+
   private
-  
+
   def build_label(build)
-    "#{build.abbreviated_label} (#{human_time(build.time)})"
+    "#{build.abbreviated_label} (#{human_time(build.time.in_time_zone)})"
   end
 
   def remove_leading_zero(string)
