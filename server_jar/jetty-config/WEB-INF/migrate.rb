2dd1a2bdfd09d67a48dd3a23b647ae4ceec797b1
Roman V. Babenko <romanvbabenko@gmail.com>
2011-09-24 14:35:49 +0300

    [-] serjer_jer was removed

--- server_jar/jetty-config/WEB-INF/migrate.rb
+++
@@ -1,4 +0,0 @@
-require File.expand_path("../rails/config/environment", __FILE__)
-ActiveRecord::Migration.verbose = true
-ActiveRecord::Migrator.migrate(File.expand_path("../rails/db/migrate", __FILE__))
-ActiveRecord::Base.clear_all_connections!
