02131ad75ced6891e519de05e57c75cb9a3f64cb
Roman V. Babenko <romanvbabenko@gmail.com>
2011-09-24 15:37:28 +0300

    [^] Default config was changed

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,7 +1,7 @@
-# This is the file that CruiseControl uses to configure its own build at 
+# This is the file that CruiseControl uses to configure its own build at
 # http://cruisecontrolrb.thoughtworks.com/.
 
 Project.configure do |project|
-  project.email_notifier.emails = ["cruisecontrolrb@thoughtworks.com"] if Socket.gethostname =~ /cruisecontrolrb.thoughtworks.com/
-  project.bundler_args = "--path=#{project.gem_install_path} --gemfile=#{project.gemfile} --no-color --local"
+  # project.email_notifier.emails = ["cruisecontrolrb@thoughtworks.com"] if Socket.gethostname =~ /cruisecontrolrb.thoughtworks.com/
+  # project.bundler_args = "--path=#{project.gem_install_path} --gemfile=#{project.gemfile} --no-color --local"
 end
